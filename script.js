2b9656192f6dcd4be6771ad8bab61c6cb70dd7ce
Руслан <r-mit@mail.ru>
2021-06-01 23:55:45 +0400

    made adjustments to the layout and posted pictures

--- script.js
+++ script.js
@@ -27,81 +27,93 @@ let active = '#sect1';
 
 /* Массивы изменяемых параметров */
 let wallMaterials = [
-    {id: 1, name: 'Абаш (горизонтально)', price: 1500, img: ''},
-    {id: 2, name: 'Абаш (вертикально)', price: 1500, img: ''},
-    {id: 3, name: 'Кедр (горизонтально)', price: 1800, img: ''},
-    {id: 4, name: 'Кедр (вертикально)', price: 1800, img: ''},
-    {id: 5, name: 'Липа (горизонтально)', price: 1200, img: ''},
-    {id: 6, name: 'Липа (вертикально)', price: 1200, img: ''},
-    {id: 7, name: 'Осина (горизонтально)', price: 1100, img: ''},
-    {id: 8, name: 'Осина (вертикально)', price: 1100, img: ''}
+    {id: 1, name: 'Абаш (горизонтально)', price: 1500, img: 'images/wallmaterials/abash_goriz.png'},
+    {id: 2, name: 'Абаш (вертикально)', price: 1500, img: 'images/wallmaterials/abash_verti.png'},
+    {id: 3, name: 'Кедр (горизонтально)', price: 1800, img: 'images/wallmaterials/kedr_goriz.png'},
+    {id: 4, name: 'Кедр (вертикально)', price: 1800, img: 'images/wallmaterials/kedr_verti.png'},
+    {id: 5, name: 'Липа (горизонтально)', price: 1200, img: 'images/wallmaterials/lipa_goriz.png'},
+    {id: 6, name: 'Липа (вертикально)', price: 1200, img: 'images/wallmaterials/lipa_verti.png'},
+    {id: 7, name: 'Осина (горизонтально)', price: 1100, img: 'images/wallmaterials/osina_goriz.png'},
+    {id: 8, name: 'Осина (вертикально)', price: 1100, img: 'images/wallmaterials/osina_verti.png'}
 ]
 let ceilingMaterials = [
-    {id: 1, name: 'Абаш', price: 1500, img: ''},
-    {id: 2, name: 'Кедр', price: 1800, img: ''},
-    {id: 3, name: 'Липа', price: 1200, img: ''},
-    {id: 4, name: 'Осина', price: 1100, img: ''}
+    {id: 1, name: 'Абаш', price: 1500, img: 'images/ceilingmaterials/potolok_abash.png'},
+    {id: 2, name: 'Кедр', price: 1800, img: 'images/ceilingmaterials/potolok_kedr.png'},
+    {id: 3, name: 'Липа', price: 1200, img: 'images/ceilingmaterials/potolok_lipa.png'},
+    {id: 4, name: 'Осина', price: 1100, img: 'images/ceilingmaterials/potolok_osina.png'}
 ]
 let doors = [
-    {id: 1, name: 'Бронза', price: 1000, img: ''},
-    {id: 2, name: 'Сатин', price: 900, img: ''},
-    {id: 3, name: 'С рисунком', price: 800, img: ''},
-    {id: 4, name: 'Фотопечать', price: 700, img: ''},
-    {id: 5, name: 'Деревянная', price: 600, img: ''}
+    {id: 1, name: 'Бронза', price: 1000, img: 'images/doors/dver_bronza.png'},
+    {id: 2, name: 'Сатин', price: 900, img: 'images/doors/dver_satin.png'},
+    {id: 3, name: 'С рисунком', price: 800, img: 'images/doors/door_picture.png'},
+    {id: 4, name: 'Фотопечать', price: 700, img: 'images/doors/door_print.png'},
+    {id: 5, name: 'Деревянная', price: 600, img: 'images/doors/door_tree.png'}
 ]
 
 let formShelvs = [
-    {id: 1, name: 'Прямые', price: 1500, img: ''},
-    {id: 2, name: 'Г-образные', price: 1400, img: ''},
-    {id: 3, name: 'П-образные', price: 1300, img: ''},
-    {id: 4, name: 'Парящие', price: 1200, img: ''}
+    {id: 1, name: 'Прямые', price: 1500, img: 'images/formshelvs/straight_shelves.png'},
+    {id: 2, name: 'Г-образные', price: 1400, img: 'images/formshelvs/g_shelves.png'},
+    {id: 3, name: 'П-образные', price: 1300, img: 'images/formshelvs/p_shelves.png'},
+    {id: 4, name: 'Парящие', price: 1200, img: 'images/formshelvs/soaring_shelves.png'}
+    //все абаш. подумать над 61 строкой
 ]
+//проверь еще раз и сконнекть
 let materialShelvs = [
-    {id: 1, name: 'Абаш', price: 1500, img: ''},
-    {id: 2, name: 'Термоабаш', price: 1300, img: ''},
-    {id: 3, name: 'Канадский кедр', price: 1200, img: ''},
-    {id: 4, name: 'Ольха', price: 1100, img: ''},
-    {id: 5, name: 'Термососна', price: 1000, img: ''}
+    {id: 1, name: 'Абаш', price: 1500, img: ['images/materialshelvs/', 'images/materialshelvs/', 'images/materialshelvs/', 'images/materialshelvs/m_soaring_abash.png']},//4 картинки разных форм полога
+    //пока здесь сделаю
+    {id: 2, name: 'Абаш + Термоабаш', price: 1300, img: ['images/materialshelvs/m_straight_termoabash.png', 'images/materialshelvs/m_g_termoabash.png', 'images/materialshelvs/m_p_termoabash.png', 'images/materialshelvs/termoabash.png']},
+    {id: 3, name: 'Канадский кедр', price: 1200, img: ['images/materialshelvs/m_straight_kedr.png', 'images/materialshelvs/m_g_kedr.png', 'images/materialshelvs/m_p_kedr.png', 'images/materialshelvs/canadian_cedar.png']},
+    {id: 4, name: 'Ольха', price: 1100, img: ['images/materialshelvs/m_straight_olha.png', 'images/materialshelvs/m_g_olha.png', 'images/materialshelvs/m_p_olha.png', 'images/materialshelvs/olha.png']},
+    {id: 5, name: 'Термососна', price: 1000, img: ['images/materialshelvs/m_straight_termososna.png', 'images/materialshelvs/m_g_termososna.png', 'images/materialshelvs/m_p_termososna.png', 'images/materialshelvs/termososna.png']}
 ]
 let backs = [
-    {id: 1, name: 'Абаш', price: 800, img: ''},
-    {id: 2, name: 'Канадский кедр', price: 700, img: ''},
-    {id: 3, name: 'Ольха', price: 600, img: ''},
-    {id: 4, name: 'Термососна', price: 500, img: ''}
+    {id: 1, name: 'Абаш', price: 800, img: 'images/backs/abash.png'},
+    {id: 2, name: 'Канадский кедр', price: 700, img: 'images/backs/canadian_cedar.png'},
+    {id: 3, name: 'Ольха', price: 600, img: 'images/backs/olha.png'},
+    {id: 4, name: 'Термососна', price: 500, img: 'images/backs/termososna.png'}
 ]
 
 let typeStoves = [
-    {id: 1, name: 'Электрическая', price: 20000, img: ''},
-    {id: 2, name: 'Дровяная', price: 15000, img: ''}
-]
-let viewStoves = [
-    {id: 1, name: 'Напольная прямоугольная', price: 3000, img: ''},
-    {id: 2, name: 'Напольная электрическая', price: 2000, img: ''},
-    {id: 3, name: 'Настенная', price: 1500, img: ''}
+    {
+        id: 1, 
+        name: 'Электрическая', 
+        viewStoves: [
+            {id: 1, name: 'Напольная прямоугольная', price: 2000, img: 'images/viewstoves/pech_el-pryam.png'},   
+            {id: 2, name: 'Напольная цилиндрическая', price: 3000, img: 'images/viewstoves/pech_el_cili.png'}
+        ]
+    },
+    {
+        id: 2, 
+        name: 'Дровяная', 
+        viewStoves: [
+            {id: 1, name: 'Напольная прямоугольная', price: 2000, img: 'images/viewstoves/pech_tree_pryam.png'},    
+            {id: 2, name: 'Напольная цилиндрическая', price: 3000, img: 'images/viewstoves/pech_tree_cili.png'}        
+        ]    
+    }
 ]
 
 let tilings = [
     {id: 1, name: 'Не выбрана', price: 4000, img: ''},
-    {id: 2, name: 'За печкой', price: 3500, img: ''},
-    {id: 3, name: 'У двери', price: 3000, img: ''},
-    {id: 4, name: 'Настенная', price: 2000, img: ''}
+    {id: 2, name: 'Талькохлорит', price: 3500, img: 'images/tilings/oblicovka_plitkoy_talk.png'},
+    {id: 3, name: 'Змеевик', price: 3000, img: 'images/tilings/oblicovka_plitkoy_zmeevik.png'},
+    {id: 4, name: 'Рваный камень', price: 2000, img: 'images/tilings/oblicovka_plitkoy_rvan.png'}
 ]
 let solts = [
     {id: 1, name: 'Не выбрана', price: 6000, img: ''},
-    {id: 2, name: 'За печкой', price: 5000, img: ''},
-    {id: 3, name: 'У двери', price: 4000, img: ''},
-    {id: 4, name: 'Настенная', price: 3000, img: ''}
+    {id: 2, name: 'За печкой', price: 5000, img: 'images/solts/gym_sol_zapechkoy.png'},
+    {id: 3, name: 'У двери', price: 4000, img: 'images/solts/gym_sol_udvery.png'},
+    {id: 4, name: 'Настенная', price: 3000, img: 'images/solts/gym_sol_podpotolok.png'}
 ]
 
 let lights = [
-    {id: 1, name: 'Светильник Германия', price: 3000, img: ''},
-    {id: 2, name: 'Светильник Финляндия', price: 2800, img: ''},
-    {id: 3, name: 'Светильник с абажуром', price: 2600, img: ''},
-    {id: 4, name: 'Цветотерапия (с пультом)', price: 2400, img: ''}
+    {id: 1, name: 'Светильник Германия', price: 3000, img: 'images/lights/svet_german.png'},
+    {id: 2, name: 'Светильник Финляндия', price: 2800, img: 'images/lights/svet_finland.png'},
+    {id: 3, name: 'Светильник с абажуром', price: 2600, img: 'images/lights/svet_abajur.png'},
+    {id: 4, name: 'Цветотерапия (с пультом)', price: 2400, img: 'images/lights/svet_therapy.png'}
 ]
 let accessories = [
-    {id: 1, name: 'Аксессуары Harvia', price: 2200, img: ''},
-    {id: 2, name: 'Аксессуары дерево', price: 2100, img: ''}
+    {id: 1, name: 'Аксессуары Harvia', price: 2200, img: 'images/accessories/accessories_harvia.png'},
+    {id: 2, name: 'Аксессуары дерево', price: 2100, img: 'images/accessories/accessories_wood.png'}
 ]
 
 /* Обработчики */
@@ -126,39 +138,68 @@ heightElem.addEventListener('change', function() {
 
 
 let wallMaterialElem = document.querySelector('#wallMaterial');
-wallMaterialElem.addEventListener('change', function() {
+wallMaterialElem.addEventListener('change', function() {    
     wallMaterial = this.value;
+    let wallImg = document.createElement('img');    
+    wallImg.src = wallMaterials.find(function(item) {if (item.id == wallMaterial) {return true}}).img;
+    wallImg.style = 'position: absolute; top: 0;';    
+    document.querySelector('#details').append(wallImg);    
     calcPrice();    
 });
 
 let ceilingMaterialElem = document.querySelector('#ceilingMaterial');
 ceilingMaterialElem.addEventListener('change', function() {
     ceilingMaterial = this.value;    
+    let ceilingImg = document.createElement('img');
+    ceilingImg.src = ceilingMaterials.find(function(item) {if (item.id == ceilingMaterial) {return true}}).img;
+    ceilingImg.style = 'position: absolute; top: 0;';
+    document.querySelector('#details').append(ceilingImg);
     calcPrice(); 
 });
 
 let doorElem = document.querySelector('#door');
 doorElem.addEventListener('change', function() {
     door = this.value; 
+    let doorImg = document.createElement('img');
+    doorImg.src = doors.find(function(item) {if (item.id == door) {return true}}).img;
+    doorImg.style = 'position: absolute; top: 0;';
+    document.querySelector('#details').append(doorImg);
     calcPrice();    
 });
-
-
+//? - если элемент существует, то идем дальше. Если нет, то все, что справа не выполняется(т.е remove не применяется).
 let formShelvElem = document.querySelector('#formShelv');
 formShelvElem.addEventListener('change', function() {
     formShelv = this.value; 
+    document.querySelector('#shelvImg')?.remove();
+    let formShelvImg = document.createElement('img');
+    formShelvImg.id = 'shelvImg'; 
+    formShelvImg.src = formShelvs.find(function(item) {if (item.id == formShelv) {return true}}).img;
+    formShelvImg.style = 'position: absolute; top: 0;'; 
+    document.querySelector('#details').append(formShelvImg);
     calcPrice();    
 });
 
 let materialShelvElem = document.querySelector('#materialShelv');
 materialShelvElem.addEventListener('change', function() {
     materialShelv = this.value;
+    document.querySelector('#shelvImg')?.remove();  
+    let materialShelvImg = document.createElement('img');
+    materialShelvImg.id = 'shelvImg';
+    materialShelvImg.src = materialShelvs.find(function(item) {if (item.id == materialShelv) {return true}}).img;
+    materialShelvImg.style = 'position: absolute; top: 0;';
+    document.querySelector('#details').append(materialShelvImg);
     calcPrice();    
 });
 
 let backElem = document.querySelector('#back');
 backElem.addEventListener('change', function() {
-    back = this.value;    
+    back = this.value; 
+    document.querySelector('#bImg')?.remove(); 
+    let backImg = document.createElement('img');
+    backImg.id = 'bImg';
+    backImg.src = backs.find(function(item) {if (item.id == back) {return true}}).img;
+    backImg.style = 'position: absolute; top: 0;';
+    document.querySelector('#details').append(backImg);
     calcPrice(); 
 });
 
@@ -166,25 +207,48 @@ backElem.addEventListener('change', function() {
 let typeStoveElem = document.querySelector('#typeStove');
 typeStoveElem.addEventListener('change', function() {
     typeStove = this.value;  
+    document.querySelector('#tStoveImg')?.remove();
+    let typeStoveImg = document.createElement('img');
+    typeStoveImg.id = 'tStoveImg';    
+    typeStoveImg.src = typeStoves.find(function(item) {if (item.id == typeStove) {return true}}).viewStoves.find(function(item) {if (item.id == viewStove) {return true}}).img;
+    typeStoveImg.style = 'position: absolute; top: 0; z-index: 1;';
+    document.querySelector('#details').append(typeStoveImg);
     calcPrice();   
 });
 
 let viewStoveElem = document.querySelector('#viewStove');
 viewStoveElem.addEventListener('change', function() {
     viewStove = this.value;   
-    calcPrice();  
+    document.querySelector('#tStoveImg')?.remove();
+    let typeStoveImg = document.createElement('img');
+    typeStoveImg.id = 'tStoveImg';    
+    typeStoveImg.src = typeStoves.find(function(item) {if (item.id == typeStove) {return true}}).viewStoves.find(function(item) {if (item.id == viewStove) {return true}}).img;
+    typeStoveImg.style = 'position: absolute; top: 0; z-index: 1;';
+    document.querySelector('#details').append(typeStoveImg);
+    calcPrice();    
 });
-
 
 let tilingElem = document.querySelector('#tiling');
 tilingElem.addEventListener('change', function() {
     tiling = this.value; 
+    document.querySelector('#tilImg')?.remove();
+    let tilingImg = document.createElement('img');
+    tilingImg.id = 'tilImg';
+    tilingImg.src = tilings.find(function(item) {if (item.id == tiling) {return true}}).img;
+    tilingImg.style = 'position: absolute; top: 0;';
+    document.querySelector('#details').append(tilingImg);
     calcPrice();    
 });
 
 let soltElem = document.querySelector('#solt');
 soltElem.addEventListener('change', function() {
     solt = this.value; 
+    document.querySelector('#soImg')?.remove();
+    let soltImg = document.createElement('img');
+    soltImg.id = 'soImg';
+    soltImg.src = solts.find(function(item) {if (item.id == solt) {return true}}).img;
+    soltImg.style = 'position: absolute; top: 0;';
+    document.querySelector('#details').append(soltImg);
     calcPrice();    
 });
 
@@ -192,12 +256,24 @@ soltElem.addEventListener('change', function() {
 let lightElem = document.querySelector('#light');
 lightElem.addEventListener('change', function() {
     light = this.value; 
+    document.querySelector('#ligImg')?.remove();
+    let lightImg = document.createElement('img');
+    lightImg.id = 'ligImg';
+    lightImg.src = lights.find(function(item) {if (item.id == light) {return true}}).img;
+    lightImg.style = 'position: absolute; top: 0;';
+    document.querySelector('#details').append(lightImg);
     calcPrice();    
 });
 
 let accessoryElem = document.querySelector('#accessory');
 accessoryElem.addEventListener('change', function() {
     accessory = this.value; 
+    document.querySelector('#accessorImg')?.remove();
+    let accessoryImg = document.createElement('img');
+    accessoryImg.id = 'accessorImg';
+    accessoryImg.src = accessories.find(function(item) {if (item.id == accessory) {return true}}).img;
+    accessoryImg.style = 'position: absolute; top: 0;';
+    document.querySelector('#details').append(accessoryImg);
     calcPrice();    
 });
 
@@ -258,7 +334,7 @@ function calcPrice() {
     /* расчет площади потолка*/
     let sCeiling = width * length;        
 
-    let price = sWall * wallMaterials.find(function(item, index, array) {if(item.id == wallMaterial){return true}}).price + sCeiling * ceilingMaterials.find(function(item, index, array) {if(item.id == ceilingMaterial){return true}}).price + doors.find(function(item, index, array) {if(item.id == door) {return true}}).price + formShelvs.find(function(item, index, array) {if(item.id == formShelv) {return true}}).price + materialShelvs.find(function(item, index, array) {if(item.id == materialShelv) {return true}}).price + backs.find(function(item, index, array) {if(item.id == back) {return true}}).price + typeStoves.find(function(item, index, array) {if(item.id == typeStove) {return true}}).price + viewStoves.find(function(item, index, array) {if(item.id == viewStove) {return true}}).price + tilings.find(function(item, index, array) {if(item.id == tiling) {return true}}).price + solts.find(function(item, index, array) {if(item.id == solt) {return true}}).price + lights.find(function(item, index, array) {if(item.id == light) {return true}}).price + accessories.find(function(item, index, array) {if(item.id == accessory) {return true}}).price + 'р.';
+    let price = sWall * wallMaterials.find(function(item, index, array) {if(item.id == wallMaterial){return true}}).price + sCeiling * ceilingMaterials.find(function(item, index, array) {if(item.id == ceilingMaterial){return true}}).price + doors.find(function(item, index, array) {if(item.id == door) {return true}}).price + formShelvs.find(function(item, index, array) {if(item.id == formShelv) {return true}}).price + materialShelvs.find(function(item, index, array) {if(item.id == materialShelv) {return true}}).price + backs.find(function(item, index, array) {if(item.id == back) {return true}}).price + typeStoves.find(function(item, index, array) {if(item.id == typeStove) {return true}}).viewStoves.find(function(item, index, array) {if(item.id == viewStove) {return true}}).price + tilings.find(function(item, index, array) {if(item.id == tiling) {return true}}).price + solts.find(function(item, index, array) {if(item.id == solt) {return true}}).price + lights.find(function(item, index, array) {if(item.id == light) {return true}}).price + accessories.find(function(item, index, array) {if(item.id == accessory) {return true}}).price + 'р.';
 
     //Обратимся по идентификатору к цене, которая в интерфейсе
     let priceElem = document.querySelector('#price');
@@ -309,17 +385,21 @@ btnNext.addEventListener('click', function() {
     active = next;
     if (active == '#sect7') {
         btnNext.classList.add('hidden');  
-        showResultParams()      
+        showResultParams();      
     }
     if (active == '#sect2') {        
         btnPrev.classList.remove('hidden');
     }
 });
 
-//зависимости у печи доделать
-//Облицовка плиткой: 
-
-
+/* Внести коррективы
+1)Дверь не сразу срабатывает
+2)форма полок: парящие - под ними другие фото - править
+раздел полки: вносить коррективы
+3)плитка!!!!!!!!!!!!!!!!!!!!!!!!!!
+- первая - не выбрана, но вверху торчит!!!!!!!
+4) materialShelvElem - удлиняется фото при нажатии только на АБАШ!(60 строка массив картинки доделай)
+5) если пройтись по далее, а затем обратно, то картинки должны пропадать
 
 /* CANVAS */
 
